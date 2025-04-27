397160f4c4904b1a17d3e4b14a91c152c7e74951
Feno <andriantsoafenojustin@gmail.com>
2025-04-27 14:46:42 +0300

    image

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 import pain from '../assets/pain.png'
+import pain2 from '../assets/555.png'
+import pain3 from '../assets/94.png'
+import pain4 from '../assets/98.png'
+import pain5 from '../assets/96.png'
+import pain6 from '../assets/95.png'
 function Card (){
 const listcard = [
     {
@@ -15,7 +20,7 @@ const listcard = [
         price: "1.800",
     },
     {
-        cars: pain,
+        cars: pain2,
         brand: "Jaguar XE L P250",
         stars: "4.8",
         review: "2.436",
@@ -26,7 +31,7 @@ const listcard = [
         price: "1.800",
     },
     {
-        cars: pain,
+        cars: pain3,
         brand: "Jaguar XE L P250",
         stars: "4.8",
         review: "2.436",
@@ -77,11 +82,11 @@ const listcard = [
           <h1 className='font-bold '>Special menu for you</h1>
           <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 place-items-center gap-6">
               <img src={pain} alt="" className='gap-4  hover:scale-105 duration-300' />
-              <img src={pain} alt="" className='gap-4  hover:scale-105 duration-300' />
-              <img src={pain} alt="" className='gap-4  hover:scale-105 duration-300' />
-              <img src={pain} alt="" className='gap-4  hover:scale-105 duration-300' />
-              <img src={pain} alt="" className='gap-4  hover:scale-105 duration-300' />
-              <img src={pain} alt="" className='gap-4  hover:scale-105 duration-300' />
+              <img src={pain6} alt="" className='gap-4  hover:scale-105 duration-300' />
+              <img src={pain3} alt="" className='gap-4  hover:scale-105 duration-300' />
+              <img src={pain4} alt="" className='gap-4  hover:scale-105 duration-300' />
+              <img src={pain5} alt="" className='gap-4  hover:scale-105 duration-300' />
+              <img src={pain2} alt="" className='gap-4  hover:scale-105 duration-300' />
            
           </div>
           
